c59d6c17bb004c278d4924f7f836c2f7e2b38e2e
tyanotyrano <tyrano09@gmail.com>
2022-01-22 23:19:13 +0900

    feat(JwtTokenProvider.java): Jwt Token Provider 클래스 작성
    Jwt Token Provider 클래스를 작성한다.
    - build.gradle : JPA, Spring Security 관련 의존성 추가
    - JwtTokenProvider : Jwt Token 을 제공하는 provider 클래스

---
+++ src/main/java/com/tyranotyrano/configuration/JwtTokenProvider.java
@@ -0,0 +1,60 @@
+package com.tyranotyrano.configuration;
+
+import java.util.Date;
+
+import org.springframework.security.core.Authentication;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JwtTokenProvider {
+    private static final String JWT_SECRET = "JWT Secret Key";
+
+    private static final int JWT_EXPIRATION_MS = 60000;
+
+    public static String generateToken(Authentication authentication) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + JWT_EXPIRATION_MS);
+
+        return Jwts.builder()
+                   .setSubject((String) authentication.getPrincipal()) // 사용자
+                   .setIssuedAt(new Date()) // 현재 시간 기반으로 생성
+                   .setExpiration(expiryDate) // 만료 시간 세팅
+                   .signWith(SignatureAlgorithm.HS512, JWT_SECRET) // 사용할 암호화 알고리즘, signature에 들어갈 secret 값 세팅
+                   .compact();
+    }
+
+    public static String getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                            .setSigningKey(JWT_SECRET)
+                            .parseClaimsJws(token)
+                            .getBody();
+
+        return claims.getSubject();
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+        return false;
+    }
+}
