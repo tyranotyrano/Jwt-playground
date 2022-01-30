cf266490a72270f59755cef8147a6e5cf57d020a
tyanotyrano <tyrano09@gmail.com>
2022-01-30 18:46:21 +0900

    feat(JwtTokenUtil.java): JWT Token 처리 관련 메서드 추가
    JWT Token 관련 유틸 클래스 기반 작업을 한다.
    - JwtTokenUtil : JWT Token 처리 관련 메서드를 추가한다.

--- src/main/java/com/tyranotyrano/configuration/JwtTokenUtil.java
+++ src/main/java/com/tyranotyrano/configuration/JwtTokenUtil.java
@@ -1,18 +1,29 @@
 package com.tyranotyrano.configuration;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtTokenUtil {
+    private static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
     @Value("${jwt.secret}")
     private String secret;
+
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
 
     public String getUsernameFromToken(String token){
         try{
@@ -27,7 +38,26 @@ public class JwtTokenUtil {
         return claimsResolver.apply(claims);
     }
 
+    public Date getExpirationDateFromToken(String token){
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token){
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    private String doGenerateToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+                   .setClaims(claims)
+                   .setSubject(username)
+                   .setIssuedAt(new Date(System.currentTimeMillis()))
+                   .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                   .signWith(SignatureAlgorithm.HS512, secret)
+                   .compact();
     }
 }
