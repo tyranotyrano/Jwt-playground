21730040a813663677499689c6ab37ef3bad9d89
tyanotyrano <tyrano09@gmail.com>
2022-02-06 23:23:05 +0900

    feat(JwtRequestFilter.java): 토큰 유효성 검사를 담당하는 JwtRequestFilter 필터 추가
    토큰 유효성 검사를 담당하는 JwtRequestFilter 필터를 추가한다.
    - JwtRequestFilter : 토큰 유효성 검사를 담당하는 Filter

---
+++ src/main/java/com/tyranotyrano/filter/JwtRequestFilter.java
@@ -0,0 +1,37 @@
+package com.tyranotyrano.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.tyranotyrano.configuration.JwtTokenUtil;
+import com.tyranotyrano.web.service.JwtUserDetailsService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtUserDetailsService jwtUserDetailsService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    public JwtRequestFilter(JwtUserDetailsService jwtUserDetailsService, JwtTokenUtil jwtTokenUtil) {
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String requestTokenHeader = request.getHeader("Authorization");
+        // TODO : Bearer Token 처리 추가할 것
+    }
+}
