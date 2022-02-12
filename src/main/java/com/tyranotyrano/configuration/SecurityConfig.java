c93bc80f5cda4ee47ec5fd549de4e58251748518
tyanotyrano <tyrano09@gmail.com>
2022-02-12 22:47:22 +0900

    feat(SecurityConfig.java): configure() 메서드 작성
    configure() 메서드를 작성한다.
    - SecurityConfig : configure() 메서드 작성

--- src/main/java/com/tyranotyrano/configuration/SecurityConfig.java
+++ src/main/java/com/tyranotyrano/configuration/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.tyranotyrano.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.tyranotyrano.filter.JwtRequestFilter;
 
@@ -35,5 +38,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.cors().disable()
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers(
+                "/authenticate",
+                "/api/v1/user",
+                "/api/v1/authenticate").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .formLogin().disable()
+            .headers().frameOptions().disable();
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
