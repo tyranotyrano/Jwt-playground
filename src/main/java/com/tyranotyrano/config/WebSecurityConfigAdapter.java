8c6e0d49db61557af3363ecbefea2df93f598f44
tyanotyrano <tyrano09@gmail.com>
2022-03-11 16:08:54 +0900

    feat(CorsConfig.java): CorsConfig 설정 추가
    CorsConfig 설정을 추가한다.
    - CorsConfig : CorsFilter Bean 을 등록한다.
    - WebSecurityConfigAdapter : CorsFilter 를 등록한다.

--- src/main/java/com/tyranotyrano/config/WebSecurityConfigAdapter.java
+++ src/main/java/com/tyranotyrano/config/WebSecurityConfigAdapter.java
@@ -5,13 +5,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilter(corsFilter);
+
         http.csrf().disable();
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
